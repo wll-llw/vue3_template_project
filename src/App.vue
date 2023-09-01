acf93899c1e17d5f41c05a5c7c3efc8246f1b530
邬鹭玲 <wululing@jz-ins.com>
2023-09-01 14:18:02 +0800

    fix: 修改文案

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Hello,560"/>
+<!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </template>
 
 <script>
